e2af5853348207c0ae2dc8a03fadab3f7a38f1a2
pedribelp <pedribelp@gmail.com>
2024-06-14 19:18:17 -0400

    Create ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,13 @@
+#Ejercicio 3 de la practica 
+
+lista = [10, 20, 30, 40, 50]
+
+# Acceder al primer elemento
+print("Primer elemento:", lista[0])
+
+# Acceder al último elemento
+print("Último elemento:", lista[-1])
+
+# Modificar el segundo elemento
+lista[1] = 25
+print("Lista después de modificar el segundo elemento:", lista)
